10c06d6feda6980cb0342ef5261e14cc69d7dd78
anastaisha <filonova.ai@phystech.edu>
2025-03-27 18:23:03 +0300

    fix: 10.06

--- fourth_hw/10.06.cpp
+++ fourth_hw/10.06.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <map>
 
-int main() {
+struct Key {
+    mutable int value;
+    Key(int v) : value(v) {}
+    
+    bool operator<(const Key& other) const {
+        return value < other.value;
+    }
+};
+
+void first_way() {
     std::map<int, std::string> myMap;
     myMap[1] = "one";
     myMap[2] = "two";
+
+    std::cout << "Before changes:" << "\n";
+    for (const auto& [key, value] : myMap) {
+        std::cout << key << ": " << value << "\n";
+    }
     
     auto it = myMap.find(1);
     if (it != myMap.end()) {
@@ -12,7 +26,34 @@ int main() {
         *keyPtr = 3;
     }
 
+    std::cout << "After changes:" << "\n";
     for (const auto& [key, value] : myMap) {
         std::cout << key << ": " << value << "\n";
     }
+}
+
+void second_way() {
+    std::map<Key, std::string> myMap;
+    myMap[Key(1)] = "one";
+    myMap[Key(2)] = "two";
+
+    std::cout << "Before changes:" << "\n";
+    for (const auto& [key, value] : myMap) {
+        std::cout << key.value << ": " << value << "\n";
+    }
+    
+    auto it = myMap.find(1);
+    if (it != myMap.end()) {
+        it->first.value = 10;
+    }
+
+    std::cout << "After changes:" << "\n";
+    for (const auto& [key, value] : myMap) {
+        std::cout << key.value << ": " << value << "\n";
+    }
+}
+
+int main() {
+    first_way();
+    second_way();
 }
